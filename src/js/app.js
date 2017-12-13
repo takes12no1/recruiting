92b134f291a9ee993a2417190a2f6b958708bd69
Mark Coleman <takes12no1@gmail.com>
2017-12-13 15:56:26 -0600

    state 2 done

--- src/js/app.js
+++ src/js/app.js
@@ -11,37 +11,124 @@ $(function() {
         console.log('Resized');
     });
 
-    function buildHome() {
-        //remove classes if previously built
-        $('#state0backgroundContainer').removeClass('backgroundContainerCollapsed');
-        $('#rightCurveShape').removeClass('fadeOut');
-        
+    function buildHome() {   
+        setTimeout(function(){     
         //add classes
+        $('#state0backgroundContainer').removeClass('backgroundContainerCollapsed');
         $('#state0backgroundContainer').addClass('backgroundContainerActive');
-        $('#rightCurveShape').addClass('fadeIn');
+        }, 1000); 
+        setTimeout(function(){
+            $('#rightCurveShape').removeClass('fadeOut');
+            $('#rightCurveShape').addClass('fadeIn');
+            $('#leftCurveImg').removeClass('slideOutLeft');
+            $('#leftCurveImg').addClass('slideInLeft');
+            $('#logo').removeClass('fadeOut');
+            $('#logo').addClass('slideInLeft');
+            $('#getInTouchBtn').removeClass('fadeOut');
+            $('#getInTouchBtn').addClass('fadeIn');
+            $('#discoverYourBestTitleImg').removeClass('fadeOut');
+            $('#discoverYourBestTitleImg').addClass('slideInLeft');
+            $('#scrollToLearnTxt').removeClass('fadeOut');
+            $('#scrollToLearnTxt').addClass('slideInLeft');
+            $('#scrollToLearnDownChevron').removeClass('animated fadeOut');
+            $('#scrollToLearnDownChevron').addClass('bounce');
+        }, 2000);    
+        //Menu
+        $('#deskTopMenuRight').addClass('hidden'); 
+        //pagination
+        $('#paginationState0').removeClass('fa-circle-thin');
+        $('#paginationState0').addClass('fa-circle');
+        homeState = 0;
         console.log('page 1 build');
     }
+
     function destroyHome() {
-        $('#state0backgroundContainer').removeClass('backgroundContainerActive');
-        $('#state0backgroundContainer').addClass('backgroundContainerCollapsed');
         $('#rightCurveShape').removeClass('fadeIn');
         $('#rightCurveShape').addClass('fadeOut');
+        $('#leftCurveImg').removeClass('slideInLeft');
+        $('#leftCurveImg').addClass('slideOutLeft');
+        $('#logo').removeClass('slideInLeft');
+        $('#logo').addClass('fadeOut');
+        $('#getInTouchBtn').removeClass('fadeIn');
+        $('#getInTouchBtn').addClass('fadeOut');
+        $('#discoverYourBestTitleImg').removeClass('slideInLeft');
+        $('#discoverYourBestTitleImg').addClass('fadeOut');
+        $('#scrollToLearnTxt').removeClass('slideInLeft');
+        $('#scrollToLearnTxt').addClass('fadeOut');
+        $('#scrollToLearnDownChevron').removeClass('bounce');
+        $('#scrollToLearnDownChevron').addClass('animated fadeOut');
+        $('#state0backgroundContainer').removeClass('backgroundContainerActive');
+        $('#state0backgroundContainer').addClass('backgroundContainerCollapsed');
+        //pagination
+        $('#paginationState0').removeClass('fa-circle');
+        $('#paginationState0').addClass('fa-circle-thin');
         console.log('page 1 destroyed');
     }
 
     function buildDouble() {
+        
+        //Background
         $('#state1backgroundContainer').removeClass('backgroundContainerCollapsed');
-        $('#wrapper').removeClass('backgroundPage1');
-
-        //add classes
         $('#state1backgroundContainer').addClass('backgroundContainerActive');
-        $('#wrapper').addClass('backgroundPage2');
+    
+        //Content
+        setTimeout(function(){
+            $('#state1TitleA').removeClass('hidden animated slideOutUp');
+            $('#state1TitleA').addClass('animated slideInDown');
+            $('#state1TitleA img').removeClass('animated fadeOut');
+            $('#state1TitleA img').addClass('animated fadeIn');
+            $('#state1TitleB').removeClass('hidden animated fadeOut');
+            $('#state1TitleB').addClass('animated fadeIn');
+            $('#state1TitleB img').removeClass('animated slideOutUp');
+            $('#state1TitleB img').addClass('animated slideInUp');
+            $('#state1Text').removeClass('hidden animated fadeOut');
+            $('#state1Text').addClass('animated fadeIn');
+            $('#state1Text p').removeClass('animated slideOutUp');
+            $('#state1Text p').addClass('animated slideInUp');
+            $('#state1Spacer').removeClass('hidden animated fadeOut');
+            $('#state1Spacer').addClass('animated fadeIn');
+            $('#state1Btn').removeClass('hidden animated slideOutLeft');
+            $('#state1Btn').addClass('animated slideInLeft');
+        }, 1000);
+        //pagination
+        $('#paginationState1').removeClass('fa-circle-thin');
+        $('#paginationState1').addClass('fa-circle');
+        //Menu
+        $('#deskTopMenuRight').removeClass('hidden');
+        homeState = 1;
         console.log('page 2 build');
     }
 
+    
+
+
+
     function destroyDouble() {
-        $('#state1backgroundContainer').removeClass('backgroundContainerActive');
-        $('#state1backgroundContainer').addClass('backgroundContainerCollapsed');
+        $('#state1TitleA').removeClass('animated slideInDown');
+        $('#state1TitleA').addClass('animated slideOutUp');
+        $('#state1TitleA img').removeClass('animated fadeIn');
+        $('#state1TitleA img').addClass('animated fadeOut');
+        
+        
+        $('#state1TitleB').removeClass('animated fadeIn');
+        $('#state1TitleB').addClass('animated fadeOut');
+        $('#state1TitleB img').removeClass('animated slideInUp');
+        $('#state1TitleB img').addClass('animated slideOutUp');
+        $('#state1Text').removeClass('animated fadeIn');
+        $('#state1Text').addClass('animated fadeOut');
+        $('#state1Text p').removeClass('animated slideInUp');
+        $('#state1Text p').addClass('animated slideOutUp');
+        $('#state1Spacer').removeClass('animated fadeIn');
+        $('#state1Spacer').addClass('animated fadeOut');
+        $('#state1Btn').removeClass('animated slideInleft');
+        $('#state1Btn').addClass('animated slideOutLeft');
+        setTimeout(function(){
+            $('#state1backgroundContainer').removeClass('backgroundContainerActive');
+            $('#state1backgroundContainer').addClass('backgroundContainerCollapsed');
+        }, 1000);
+        //pagination
+        $('#paginationState1').removeClass('fa-circle');
+        $('#paginationState1').addClass('fa-circle-thin');
         console.log('page 2 destroy');
     }
 
@@ -50,12 +137,22 @@ $(function() {
         
         //add classes
         $('#state2backgroundContainer').addClass('backgroundContainerActive');
+        //pagination
+        $('#paginationState2').removeClass('fa-circle-thin');
+        $('#paginationState2').addClass('fa-circle');
+        //Menu
+        $('#deskTopMenuRight').removeClass('hidden');
+        homeState = 2;
         console.log('page 3 build');
     }
 
     function destroyJoin() {
         $('#state2backgroundContainer').removeClass('backgroundContainerActive');
         $('#state2backgroundContainer').addClass('backgroundContainerCollapsed');
+        
+        //pagination
+        $('#paginationState2').removeClass('fa-circle');
+        $('#paginationState2').addClass('fa-circle-thin');
         console.log('page 3 destroy');
     }
 
@@ -64,11 +161,21 @@ $(function() {
         
         //add classes
         $('#state3backgroundContainer').addClass('backgroundContainerActive');
+        //pagination
+        $('#paginationState3').removeClass('fa-circle-thin');
+        $('#paginationState3').addClass('fa-circle');
+        //Menu
+        $('#deskTopMenuRight').removeClass('hidden');
+        homeState = 3;
         console.log('page 4 build');
     }
+
     function destroyOperate() {
         $('#state3backgroundContainer').removeClass('backgroundContainerActive');
         $('#state3backgroundContainer').addClass('backgroundContainerCollapsed');
+        //pagination
+        $('#paginationState3').removeClass('fa-circle');
+        $('#paginationState3').addClass('fa-circle-thin');
         console.log('page 4 destroy');
     } 
 
@@ -77,32 +184,119 @@ $(function() {
         
         //add classes
         $('#state4backgroundContainer').addClass('backgroundContainerActive');
+        //pagination
+        $('#paginationState4').removeClass('fa-circle-thin');
+        $('#paginationState4').addClass('fa-circle');
+        $('#paginationDesktop i').addClass('bluePagination');
+        //Menu
+        $('#deskTopMenuRight').removeClass('hidden');
+        homeState = 4;
         console.log('page 5 build');
     }
 
     function destroyWinning() {
         $('#state4backgroundContainer').removeClass('backgroundContainerActive');
         $('#state4backgroundContainer').addClass('backgroundContainerCollapsed');
+        //pagination
+        $('#paginationState4').removeClass('fa-circle');
+        $('#paginationState4').addClass('fa-circle-thin');
+        $('#paginationDesktop i').removeClass('bluePagination');
         console.log('page 5 destroy');
     }
+
     function buildLastPage() {
         $('#state5backgroundContainer').removeClass('backgroundContainerCollapsed');
         
         //add classes
         $('#state5backgroundContainer').addClass('backgroundContainerActive');
+        //pagination
+        $('#paginationState5').removeClass('fa-circle-thin');
+        $('#paginationState5').addClass('fa-circle');
+        homeState = 5;
         console.log('page 6 build');
     }
 
     function destroyLastPage() {
         $('#state5backgroundContainer').removeClass('backgroundContainerActive');
         $('#state5backgroundContainer').addClass('backgroundContainerCollapsed');
+        //pagination
+        $('#paginationState5').removeClass('fa-circle');
+        $('#paginationState5').addClass('fa-circle-thin');
+        //Menu
+        $('#deskTopMenuRight').removeClass('hidden');
         console.log('page 6 destroy');
     }
+
+    // pagination interaction
+    $('#paginationState0').on('click', function(){
+        changeState(0);
+    });
+    $('#paginationState1').on('click', function(){
+        changeState(1);
+    });
+    $('#paginationState2').on('click', function(){
+        changeState(2);
+    });
+    $('#paginationState3').on('click', function(){
+        changeState(3);
+    });
+    $('#paginationState4').on('click', function(){
+        changeState(4);
+    });
+    $('#paginationState5').on('click', function(){
+        changeState(5);
+    });
+
+    function changeState(goToState) {
+        var currentState = homeState;
+        //destroy Previous
+        switch (currentState) {
+            case 0:
+                destroyHome();
+                break;
+            case 1:
+                destroyDouble();
+                break;
+            case 2:
+                destroyJoin();
+                break;
+            case 3:
+                destroyOperate();
+                break;
+            case 4:
+                destroyWinning();
+                break;
+            case 5:
+                destroyLastPage();
+        }
+        // Build new
+        switch (goToState) {
+            case 0:
+                buildHome();
+                break;
+            case 1:
+                buildDouble();
+                break;
+            case 2:
+                buildJoin();
+                break;
+            case 3:
+                buildOperate();
+                break;
+            case 4:
+                buildWinning();
+                break;
+            case 5:
+                buildLastPage();
+        }
+    }
+
+
 
     function changeHomeCase(x) {
         var prevHomeState = homeState;
         homeState = homeState + x;
-        console.log('prev= ' + prevHomeState + ' - Current= ' + homeState);
+        // console.log('prev= ' + prevHomeState + ' - Current= ' + homeState);
     
         if (homeState >= (stateCasesTotal + 1)) {
             homeState = 0;
